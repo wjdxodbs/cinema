ee808bf0b2b207cfc5de4575b062c944c1bbb5ee
wjdxodbs <wjdxodbs52@naver.com>
2026-02-23 01:01:35 +0900

    chore: 프로젝트 초기 설정 및 의존성 구성

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
